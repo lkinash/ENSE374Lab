70dbcdafd2d138b95eacc4e10d158a6aed8067bf
Lindsey Kinash <kinash.lindsey@gmail.com>
2015-10-26 21:30:15 -0600

    Comments added to explain the initial bits of code

--- ENSE374_Lab3/src/LinkedList.java
+++ ENSE374_Lab3/src/LinkedList.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 public class LinkedList {
 
+	private ListElement head = null; 	//create a head node for the list
+	private ListElement end = null;		//create an end node for the list to have nodes added to
 	
-	
-	public static void main(String[] args)
+	public static void main(String[] args)	
 	{
-		ListElement le = new ListElement();
-		le.setData(5);
+			
+		
 	}
 
 	public void addElement(ListElement le)
--- ENSE374_Lab3/src/ListElement.java
+++ ENSE374_Lab3/src/ListElement.java
@@ -2,25 +2,25 @@
 public class ListElement {
 
 	
-		private ListElement next;
-		private ListElement previous;
-		private int data;
+		private ListElement next;			//pointer for the next node
+		private ListElement previous;		//pointer to the last node, to be used in the doubley linked list only 
+		private int data;					//where the data in the node is stored
 		
-		public ListElement( )
-		{
-			this.data = 0;
-			this.next = null;
-			this.previous = null;
+		public ListElement( )		//a list element object constructor
+		{	
+			this.data = 0;			//sets the data to zero
+			this.next = null;		//sets the next pointer to null
+			this.previous = null;	//sets the previous pointer to null as well, this will stay null the entire time in the single linked list
 		}
 	
-		public void setData( int data ) 
+		public void setData( int data ) 		//a setter for the list element object
 		{
-			this.data = data ;
+			this.data = data ;			//sets the value of the data element for the node
 		}
 	
-		public int getData ( ) 
+		public int getData ( ) 			//a getter for the list element object 
 		{
-			return this.data ;
+			return this.data ;			//returns the value of the data in the node
 		}
 	
 }
