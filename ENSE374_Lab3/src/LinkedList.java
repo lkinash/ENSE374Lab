1fd13285b29a2f563ac71a79bf9fd08dde8bcb9b
Lindsey Kinash <kinash.lindsey@gmail.com>
2015-10-26 22:51:14 -0600

    Testing of the list, the other functions work well but the add element function only works when a new element is created outside of the function

--- ENSE374_Lab3/src/LinkedList.java
+++ ENSE374_Lab3/src/LinkedList.java
@@ -4,8 +4,7 @@ public class LinkedList {
 
 	private ListElement head;  	//this is the creation of a head node for the list
 	private ListElement end;		//this is a end node for the list for new nodes to be added
-	private ListElement temp;		//a tempoaray element to be used as a marker when moving through the list	
-	private ListElement setter;
+	private ListElement temp;		//a temperay element to be used as a marker when moving through the list	
 	private int listCount;
 	
 	
@@ -14,7 +13,6 @@ public class LinkedList {
 		head = null;		//sets the head to be a null node 
 		end = null;				//sets the last element in the list to be null as well
 		temp = null;			//initializes the temp
-		setter = null;			//initializes the setter
 		listCount = 0;			//sets the list counter to be null too 
 	}
 	
@@ -29,39 +27,48 @@ public class LinkedList {
 		linked.printLinkedListHead();
 		linked.printLinkedListTail();
 		
-		//ListElement le2 = new ListElement();
-		//le2.setData(67);
-		//linked.addElement(le2);
+		ListElement le2 = new ListElement();
+		le2.setData(67);
+		linked.addElement(le2);
 
-		le.setData(242);
-		linked.addElement(le);
+		linked.printLinkedListHead();
+		linked.printLinkedListTail();
+		
+		ListElement le3 = new ListElement();
+		le3.setData(242);
+		linked.addElement(le3);
 		
 		linked.printLinkedListHead();
 		linked.printLinkedListTail();
 		
-		le.setData(55);
-		linked.addElement(le);
+		ListElement le4 = new ListElement();
+		le4.setData(55);
+		linked.addElement(le4);
 
 		linked.printLinkedListHead();
 		linked.printLinkedListTail();
+		
+		int a = linked.getElement(0).getData();
+		System.out.println(a);
+		
 		
 	}
 
 	public void addElement(ListElement le)			//adds an element to the list
 	{
-		//ListElement element = new ListElement();
+		//ListElement setter = new ListElement();
 		
-		setter = le;
+		//setter = le;
 		
 		if(head == null)				//checks if there is a head node in the list
 		{
-			head = setter;				//adds that element to be the head of the list
+			head = le;				//adds that element to be the head of the list
 			end = head;				//since there is only one node, it is the head and thus that is the end too 
 		}
 		else							//if there is a head node in the list then add the new one to the end
 		{
-			end.setNext(setter);			//sets the pointer so that the next element is the new node 
-			end = setter;					//sets it so that the end is now the new element
+			end.setNext(le);			//sets the pointer so that the next element is the new node 
+			end = le;					//sets it so that the end is now the new element
 		}
 		listCount++;  			//increments the list count
 		return;
