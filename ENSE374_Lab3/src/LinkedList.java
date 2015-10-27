ee17cf68b23c637c9898116a664c52f05e4469b7
Lindsey Kinash <kinash.lindsey@gmail.com>
2015-10-26 22:38:26 -0600

    Beginning code testing

--- ENSE374_Lab3/src/LinkedList.java
+++ ENSE374_Lab3/src/LinkedList.java
@@ -5,6 +5,7 @@ public class LinkedList {
 	private ListElement head;  	//this is the creation of a head node for the list
 	private ListElement end;		//this is a end node for the list for new nodes to be added
 	private ListElement temp;		//a tempoaray element to be used as a marker when moving through the list	
+	private ListElement setter;
 	private int listCount;
 	
 	
@@ -12,6 +13,8 @@ public class LinkedList {
 	{
 		head = null;		//sets the head to be a null node 
 		end = null;				//sets the last element in the list to be null as well
+		temp = null;			//initializes the temp
+		setter = null;			//initializes the setter
 		listCount = 0;			//sets the list counter to be null too 
 	}
 	
@@ -20,21 +23,45 @@ public class LinkedList {
 		LinkedList linked = new LinkedList();
 		
 		ListElement le = new ListElement();
-		
+		le.setData(34);
+		linked.addElement(le);
 
+		linked.printLinkedListHead();
+		linked.printLinkedListTail();
+		
+		//ListElement le2 = new ListElement();
+		//le2.setData(67);
+		//linked.addElement(le2);
+
+		le.setData(242);
+		linked.addElement(le);
+		
+		linked.printLinkedListHead();
+		linked.printLinkedListTail();
+		
+		le.setData(55);
+		linked.addElement(le);
+
+		linked.printLinkedListHead();
+		linked.printLinkedListTail();
+		
 	}
 
 	public void addElement(ListElement le)			//adds an element to the list
 	{
+		//ListElement element = new ListElement();
+		
+		setter = le;
+		
 		if(head == null)				//checks if there is a head node in the list
 		{
-			head = le;				//adds that element to be the head of the list
+			head = setter;				//adds that element to be the head of the list
 			end = head;				//since there is only one node, it is the head and thus that is the end too 
 		}
 		else							//if there is a head node in the list then add the new one to the end
 		{
-			end.setNext(le);			//sets the pointer so that the next element is the new node 
-			end = le;					//sets it so that the end is now the new element
+			end.setNext(setter);			//sets the pointer so that the next element is the new node 
+			end = setter;					//sets it so that the end is now the new element
 		}
 		listCount++;  			//increments the list count
 		return;
@@ -49,15 +76,15 @@ public class LinkedList {
 		
 		for(int i = 0; i < index; i++)		//then iterating through until it gets to the index, this is done using a for loop
 		{
-			
+			temp = temp.getNext(temp);		//the temp is then set to the next node in the list
 		}
-		return;
+		return temp;						//then the temp is returned to the caller
 	}
 
 	public ListElement deleteElement(int index)
 	{
 		
-		return;
+		return temp;
 	}
 	
 	public void printLinkedListTail()			//this method prints the data value in the last element in the list
