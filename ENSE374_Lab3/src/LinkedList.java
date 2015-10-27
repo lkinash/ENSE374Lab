860fbdf094238e7c77dffadbc77622b8e8706529
Lindsey Kinash <kinash.lindsey@gmail.com>
2015-10-26 21:06:47 -0600

    Prototypes for the functions required

--- ENSE374_Lab3/src/LinkedList.java
+++ ENSE374_Lab3/src/LinkedList.java
@@ -9,5 +9,31 @@ public class LinkedList {
 		ListElement le = new ListElement();
 		le.setData(5);
 	}
+
+	public void addElement(ListElement le)
+	{
+		return;
+	}
 	
+	public ListElement getElement(int index)
+	{
+		return ListElement;
+	}
+
+	public ListElement deleteElement(int index)
+	{
+		return ListElement;
+	}
+	
+	public void printLinkedListTail()
+	{
+		
+		return;
+	}
+	
+	public void printLinkedListHead()
+	{
+		
+		return;
+	}
 }
