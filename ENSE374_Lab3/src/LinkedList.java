45870598ebac5aa7055ddc1cd22b10207f569804
Lindsey Kinash <kinash.lindsey@gmail.com>
2015-10-26 21:44:03 -0600

    Implement the print out functions

--- ENSE374_Lab3/src/LinkedList.java
+++ ENSE374_Lab3/src/LinkedList.java
@@ -2,39 +2,43 @@ import java.util.*;
 
 public class LinkedList {
 
-	private ListElement head = null; 	//create a head node for the list
-	private ListElement end = null;		//create an end node for the list to have nodes added to
+	private ListElement head = null; 	//this is the creation of a head node for the list
+	private ListElement end = null;		//this is a end node for the list for new nodes to be added
 	
-	public static void main(String[] args)	
+	public static void main(String[] args)
 	{
-			
-		
+		ListElement le = new ListElement();
+
 	}
 
 	public void addElement(ListElement le)
 	{
+		
+		le.setData(5);
 		return;
 	}
 	
 	public ListElement getElement(int index)
 	{
-		return ListElement;
+		
+		return;
 	}
 
 	public ListElement deleteElement(int index)
-	{
-		return ListElement;
-	}
-	
-	public void printLinkedListTail()
 	{
 		
 		return;
 	}
 	
-	public void printLinkedListHead()
+	public void printLinkedListTail()			//this method prints the data value in the last element in the list
 	{
-		
+		System.out.println("The tail of the list contains the value: " + end.getData());		//prints out the data value at the end of the list
+		return;
+	}
+	
+	public void printLinkedListHead()				//this method prints the data value in the first element in the list
+	{
+		System.out.println("The head of the list contains the value: " + head.getData());		//prints out the data value at the head of the list
 		return;
 	}
 }
